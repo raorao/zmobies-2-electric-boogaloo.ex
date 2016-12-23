a1a5726190e613bbf6ef75382b70be686d2cbd98
Srinivas Rao <savinirs@gmail.com>
2016-12-23 15:33:31 -0800

    suport for limits

--- lib/zmobies/world.ex
+++ lib/zmobies/world.ex
@@ -1,10 +1,11 @@
 defmodule Zmobies.World do
+  use GenServer
   @doc ~S"""
     Module for managing global locations of beings.
 
     ## Examples
 
-        iex> Zmobies.World.init
+        iex> Zmobies.World.start_link(10, 10)
         iex> Zmobies.World.at(Zmobies.Location.at(x: 1, y: 1))
         :vacant
         iex> Zmobies.World.insert(Zmobies.Location.at(x: 1, y: 1), :human)
@@ -23,35 +24,98 @@ defmodule Zmobies.World do
 
   alias Zmobies.Location
 
-  def init do
-    :ets.new(:world, [:set, :named_table])
+  def start_link(x_lim, y_lim) do
+    :ets.new(:world, [:set, :named_table, :public])
+
+    GenServer.start_link(
+      __MODULE__,
+      {x_lim, y_lim},
+      name: :world
+    )
   end
 
-  def insert(%Location{} = location, value) do
-    case :ets.insert_new(:world, {location, value}) do
-      true -> {:ok, value}
-      false -> at(location)
-    end
+  def stop do
+    GenServer.stop(:world)
   end
 
   def at(%Location{} = location) do
+    GenServer.call(:world, {:at, location})
+  end
+
+  def insert(%Location{} = location, value) do
+    GenServer.call(:world, {:insert, location, value})
+  end
+
+  def remove(%Location{} = location) do
+    GenServer.call(:world, {:remove, location})
+  end
+
+  def move(%Location{} = from, %Location{} = to) do
+    GenServer.call(:world, {:move, from, to})
+  end
+
+  def handle_call({:at, location}, _, state) do
+    {:reply, get(location, state), state}
+  end
+
+  def handle_call({:insert, location, value}, _, state) do
+    {:reply, create(location, value, state), state}
+  end
+
+  def handle_call({:remove, location}, _, state) do
+    {:reply, delete(location, state), state}
+  end
+
+  def handle_call({:move, from, to}, _, state) do
+    {:reply, update(from, to, state), state}
+  end
+
+  defmacro out_of_bounds(x, y, x_lim, y_lim) do
+    quote do: 0 > unquote(x) or unquote(x) >= unquote(x_lim) or 0 > unquote(y) or unquote(y) >= unquote(y_lim)
+  end
+
+  defp get(%Location{x: x, y: y}, {x_lim, y_lim}) when out_of_bounds(x, y, x_lim, y_lim) do
+    :out_of_bounds
+  end
+
+  defp get(location, _limits) do
     case :ets.lookup(:world, location) do
       [{^location, value}] -> {:occupied, value}
       [] -> :vacant
     end
   end
 
-  def remove(%Location{} = location) do
+  defp create(%Location{x: x, y: y}, _, {x_lim, y_lim}) when out_of_bounds(x, y, x_lim, y_lim) do
+    :out_of_bounds
+  end
+
+  defp create(location, value, limits) do
+    case :ets.insert_new(:world, {location, value}) do
+      true -> {:ok, value}
+      false -> get(location, limits)
+    end
+  end
+
+  defp delete(%Location{x: x, y: y}, {x_lim, y_lim}) when out_of_bounds(x, y, x_lim, y_lim) do
+    :out_of_bounds
+  end
+
+  defp delete(location, _limits) do
     case :ets.delete(:world, location) do
       true -> :ok
       false -> :vacant
     end
   end
 
-  def move(%Location{} = from, %Location{} = to) do
-    {:occupied, value} = at(from)
-    case insert(to, value) do
-      {:ok, ^value} -> remove(from)
+  defp update(%Location{x: x, y: y}, %Location{x: new_x, y: new_y}, {x_lim, y_lim}) when out_of_bounds(x, y, x_lim, y_lim) or out_of_bounds(new_x, new_y, x_lim, y_lim)   do
+    :out_of_bounds
+  end
+
+  defp update(from, to, state) do
+    {:occupied, value} = get(from, state)
+
+    case create(to, value, state) do
+      {:ok, ^value} -> delete(from, state)
       {:occupied, value} -> {:occupied, value}
     end
   end
--- test/lib/world_test.exs
+++ test/lib/world_test.exs
@@ -4,43 +4,63 @@ defmodule ZmobiesWorldTest do
   doctest World
 
   test "correctly idenfies vacant spaces" do
-    World.init
+    World.start_link(10, 10)
     assert World.at(Location.at(x: 1, y: 1)) == :vacant
+    World.stop
   end
 
   test "can insert into vacant entry" do
-    World.init
+    World.start_link(10, 10)
     assert World.insert(Location.at(x: 1, y: 1), :zombie) == {:ok, :zombie}
     assert World.at(Location.at(x: 1, y: 1)) == {:occupied, :zombie}
   end
 
   test "cannot insert into occupied entry" do
-    World.init
+    World.start_link(10, 10)
     World.insert Location.at(x: 1, y: 1), :zombie
     assert World.insert(Location.at(x: 1, y: 1), :human) == {:occupied, :zombie}
+    World.stop
+  end
+
+  test "cannot insert into out of bounds area" do
+    World.start_link(10, 10)
+    assert World.at(Location.at(x: 11, y: 1))  == :out_of_bounds
+    assert World.at(Location.at(x: 1, y: 11)) == :out_of_bounds
+    assert World.at(Location.at(x: -1, y: 1)) == :out_of_bounds
+    assert World.at(Location.at(x: 1, y: -1)) == :out_of_bounds
+    assert World.insert(Location.at(x: 11, y: 1), :zombie) == :out_of_bounds
+    assert World.remove(Location.at(x: 11, y: 1)) == :out_of_bounds
+    assert World.move(Location.at(x: 1, y: 1), Location.at(x: 11, y: 1)) == :out_of_bounds
+    # assert World.move(Location.at(x: 1, y: 11), Location.at(x: 1, y: 1)) == :out_of_bounds
+    World.stop
   end
 
   test "can delete entry" do
-    World.init
+    World.start_link(10, 10)
     World.insert Location.at(x: 1, y: 1), :zombie
     World.remove Location.at(x: 1, y: 1)
     assert World.at(Location.at(x: 1, y: 1)) == :vacant
+    World.stop
+
   end
 
   test "can move entry from one location to another" do
-    World.init
+    World.start_link(10, 10)
     World.insert Location.at(x: 1, y: 1), :zombie
     assert World.move(Location.at(x: 1, y: 1), Location.at(x: 1, y: 2)) == :ok
     assert World.at(Location.at(x: 1, y: 1)) == :vacant
     assert World.at(Location.at(x: 1, y: 2)) == {:occupied, :zombie}
+    World.stop
+
   end
 
   test "can't move entry to occupied location" do
-    World.init
+    World.start_link(10, 10)
     World.insert Location.at(x: 1, y: 1), :zombie
     World.insert Location.at(x: 1, y: 2), :human
     assert World.move(Location.at(x: 1, y: 1), Location.at(x: 1, y: 2)) == {:occupied, :human}
     assert World.at(Location.at(x: 1, y: 1)) == {:occupied, :zombie}
     assert World.at(Location.at(x: 1, y: 2)) == {:occupied, :human}
+    World.stop
   end
 end
