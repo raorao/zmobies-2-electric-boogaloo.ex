ab2f1708108cfc6f90efdffd5469f03fc54be393
Srinivas Rao <savinirs@gmail.com>
2016-12-23 14:42:38 -0800

    don't allow upserts

--- lib/zmobies/world.ex
+++ lib/zmobies/world.ex
@@ -7,15 +7,15 @@ defmodule Zmobies.World do
         iex> Zmobies.World.init
         iex> Zmobies.World.at(Zmobies.Location.at(x: 1, y: 1))
         :vacant
-        iex> Zmobies.World.upsert(Zmobies.Location.at(x: 1, y: 1), :human)
+        iex> Zmobies.World.insert(Zmobies.Location.at(x: 1, y: 1), :human)
+        {:ok, :human}
         iex> Zmobies.World.at(Zmobies.Location.at(x: 1, y: 1))
         {:occupied, :human}
-        iex> Zmobies.World.upsert(Zmobies.Location.at(x: 1, y: 1), :zombie)
-        iex> Zmobies.World.at(Zmobies.Location.at(x: 1, y: 1))
-        {:occupied, :zombie}
+        iex> Zmobies.World.insert(Zmobies.Location.at(x: 1, y: 1), :zombie)
+        {:occupied, :human}
         iex> Zmobies.World.move(Zmobies.Location.at(x: 1, y: 1), Zmobies.Location.at(x: 1, y: 2))
         iex> Zmobies.World.at(Zmobies.Location.at(x: 1, y: 2))
-        {:occupied, :zombie}
+        {:occupied, :human}
         iex> Zmobies.World.remove(Zmobies.Location.at(x: 1, y: 1))
         iex> Zmobies.World.at(Zmobies.Location.at(x: 1, y: 1))
         :vacant
@@ -27,8 +27,11 @@ defmodule Zmobies.World do
     :ets.new(:world, [:set, :named_table])
   end
 
-  def upsert(%Location{} = location, value) do
-    :ets.insert(:world, {location, value})
+  def insert(%Location{} = location, value) do
+    case :ets.insert_new(:world, {location, value}) do
+      true -> {:ok, value}
+      false -> at(location)
+    end
   end
 
   def at(%Location{} = location) do
@@ -39,12 +42,17 @@ defmodule Zmobies.World do
   end
 
   def remove(%Location{} = location) do
-    :ets.delete(:world, location)
+    case :ets.delete(:world, location) do
+      true -> :ok
+      false -> :vacant
+    end
   end
 
   def move(%Location{} = from, %Location{} = to) do
     {:occupied, value} = at(from)
-    upsert(to, value)
-    remove(from)
+    case insert(to, value) do
+      {:ok, ^value} -> remove(from)
+      {:occupied, value} -> {:occupied, value}
+    end
   end
 end
--- test/lib/world_test.exs
+++ test/lib/world_test.exs
@@ -8,31 +8,39 @@ defmodule ZmobiesWorldTest do
     assert World.at(Location.at(x: 1, y: 1)) == :vacant
   end
 
-  test "can insert and read entry from map" do
+  test "can insert into vacant entry" do
     World.init
-    World.upsert Location.at(x: 1, y: 1), :zombie
+    assert World.insert(Location.at(x: 1, y: 1), :zombie) == {:ok, :zombie}
     assert World.at(Location.at(x: 1, y: 1)) == {:occupied, :zombie}
   end
 
-  test "can update entry from map" do
+  test "cannot insert into occupied entry" do
     World.init
-    World.upsert Location.at(x: 1, y: 1), :zombie
-    World.upsert Location.at(x: 1, y: 1), :human
-    assert World.at(Location.at(x: 1, y: 1)) == {:occupied, :human}
+    World.insert Location.at(x: 1, y: 1), :zombie
+    assert World.insert(Location.at(x: 1, y: 1), :human) == {:occupied, :zombie}
   end
 
-  test "can delete entry from map" do
+  test "can delete entry" do
     World.init
-    World.upsert Location.at(x: 1, y: 1), :zombie
+    World.insert Location.at(x: 1, y: 1), :zombie
     World.remove Location.at(x: 1, y: 1)
     assert World.at(Location.at(x: 1, y: 1)) == :vacant
   end
 
-  test "can move being from one location to another" do
+  test "can move entry from one location to another" do
     World.init
-    World.upsert Location.at(x: 1, y: 1), :zombie
-    World.move Location.at(x: 1, y: 1), Location.at(x: 1, y: 2)
+    World.insert Location.at(x: 1, y: 1), :zombie
+    assert World.move(Location.at(x: 1, y: 1), Location.at(x: 1, y: 2)) == :ok
     assert World.at(Location.at(x: 1, y: 1)) == :vacant
     assert World.at(Location.at(x: 1, y: 2)) == {:occupied, :zombie}
+  end
+
+  test "can't move entry to occupied location" do
+    World.init
+    World.insert Location.at(x: 1, y: 1), :zombie
+    World.insert Location.at(x: 1, y: 2), :human
+    assert World.move(Location.at(x: 1, y: 1), Location.at(x: 1, y: 2)) == {:occupied, :human}
+    assert World.at(Location.at(x: 1, y: 1)) == {:occupied, :zombie}
+    assert World.at(Location.at(x: 1, y: 2)) == {:occupied, :human}
   end
 end
