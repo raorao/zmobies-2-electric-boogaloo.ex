3b598a0ac6b20162a77d7ffc80d47194b1397680
Srinivas Rao <savinirs@gmail.com>
2016-12-26 09:09:42 -0800

    implements towards and away_from

--- lib/zmobies/movement.ex
+++ lib/zmobies/movement.ex
@@ -38,6 +38,31 @@ defmodule Zmobies.Movement do
     end
   end
 
+  def towards(target, current) do
+    all_possible_locations(current)
+    |> Enum.filter(fn(location) -> distance(current, target) >= distance(location, target) end)
+    |> Enum.shuffle
+  end
+
+  def away_from(target, current) do
+    all_possible_locations(current)
+    |> Enum.filter(fn(location) -> distance(current, target) <= distance(location, target) end)
+    |> Enum.shuffle
+  end
+
+
+  defp all_possible_locations(current = %Location{x: x, y: y}) do
+    all_locations  = for new_x <- (x - 1)..(x + 1), new_y <- (y - 1)..(y + 1) do
+      Location.at(x: new_x, y: new_y)
+    end
+
+    all_locations -- [current]
+  end
+
+  defp distance(%Location{x: target_x, y: target_y}, %Location{x: current_x, y: current_y}) do
+    abs(target_x - current_x) + abs(target_y - current_y)
+  end
+
   def nearest_enemy(proximity_stream, being) do
     proximity_stream
     |> Stream.take(10)
--- test/lib/movement_test.exs
+++ test/lib/movement_test.exs
@@ -132,4 +132,106 @@ defmodule MovementTest do
       assert Movement.nearest_enemy(stream, human) == {near_enemy_location, near_enemy}
     end
   end
+
+  describe "towards" do
+    test "returns a list of possible moves that do not move away from target (0 directions)" do
+      WorldManager.start_link(10,10)
+      location = Location.at(x: 1, y: 1)
+      WorldManager.insert(location, :human)
+      target_location = Location.at(x: 1, y: 1)
+
+      assert Movement.towards(target_location, location) == []
+    end
+
+    test "returns a list of possible moves that do not move away from target (1 direction)" do
+      WorldManager.start_link(10,10)
+      location = Location.at(x: 1, y: 1)
+      WorldManager.insert(location, :human)
+      target_location = Location.at(x: 1, y: 3)
+
+      possible_moves = [
+        Location.at(x: 0, y: 2),
+        Location.at(x: 2, y: 2),
+        Location.at(x: 1, y: 2)
+      ] |> Enum.sort
+
+      assert Movement.towards(target_location, location) |> Enum.sort == possible_moves
+    end
+
+    test "returns a list of possible moves that do not move away from target (2 directions)" do
+      WorldManager.start_link(10,10)
+      location = Location.at(x: 1, y: 1)
+      WorldManager.insert(location, :human)
+      target_location = Location.at(x: 3, y: 3)
+
+      possible_moves = [
+        Location.at(x: 0, y: 2),
+        Location.at(x: 1, y: 2),
+        Location.at(x: 2, y: 0),
+        Location.at(x: 2, y: 1),
+        Location.at(x: 2, y: 2),
+
+      ] |> Enum.sort
+
+      assert Movement.towards(target_location, location) |> Enum.sort == possible_moves
+    end
+  end
+
+  describe "away_from" do
+    test "returns a list of possible moves that do not move towards target (0 directions)" do
+      WorldManager.start_link(10,10)
+      location = Location.at(x: 2, y: 2)
+      WorldManager.insert(location, :human)
+      target_location = Location.at(x: 2, y: 2)
+
+      possible_moves = [
+        Location.at(x: 3, y: 3),
+        Location.at(x: 2, y: 3),
+        Location.at(x: 1, y: 3),
+        Location.at(x: 3, y: 2),
+        Location.at(x: 1, y: 2),
+        Location.at(x: 3, y: 1),
+        Location.at(x: 2, y: 1),
+        Location.at(x: 1, y: 1),
+      ] |> Enum.sort
+
+      assert Movement.away_from(target_location, location) |> Enum.sort == possible_moves
+    end
+
+    test "returns a list of possible moves that do not move towards target (1 direction)" do
+      WorldManager.start_link(10,10)
+      location = Location.at(x: 2, y: 2)
+      WorldManager.insert(location, :human)
+      target_location = Location.at(x: 3, y: 2)
+
+      possible_moves = [
+        Location.at(x: 3, y: 3),
+        Location.at(x: 2, y: 3),
+        Location.at(x: 1, y: 3),
+        Location.at(x: 1, y: 2),
+        Location.at(x: 3, y: 1),
+        Location.at(x: 2, y: 1),
+        Location.at(x: 1, y: 1),
+      ] |> Enum.sort
+
+      assert Movement.away_from(target_location, location) |> Enum.sort == possible_moves
+    end
+
+    test "returns a list of possible moves that do not move towards target (2 directions)" do
+      WorldManager.start_link(10,10)
+      location = Location.at(x: 2, y: 2)
+      WorldManager.insert(location, :human)
+      target_location = Location.at(x: 3, y: 3)
+
+      possible_moves = [
+        Location.at(x: 1, y: 3),
+        Location.at(x: 1, y: 2),
+        Location.at(x: 3, y: 1),
+        Location.at(x: 2, y: 1),
+        Location.at(x: 1, y: 1),
+      ] |> Enum.sort
+
+      assert Movement.away_from(target_location, location) |> Enum.sort == possible_moves
+    end
+  end
 end
