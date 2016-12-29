ce2a3e992438a6fdf83c5c6629df862903eee1ef
Srinivas Rao <savinirs@gmail.com>
2016-12-29 10:53:57 -0800

    heroku config

--- apps/web_client/web/channels/user_socket.ex
+++ apps/web_client/web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule WebClient.UserSocket do
   channel "game:*", WebClient.GameChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
