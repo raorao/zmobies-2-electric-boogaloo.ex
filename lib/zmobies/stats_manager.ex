1f565d49bb9633e2652d8d68b365e671cbcbb689
Srinivas Rao <savinirs@gmail.com>
2016-12-28 17:00:08 -0800

    stats manager has more safeguards

--- lib/zmobies/stats_manager.ex
+++ lib/zmobies/stats_manager.ex
@@ -5,7 +5,7 @@ defmodule Zmobies.StatsManager do
   def start_link do
     GenServer.start_link(
       __MODULE__,
-      :ongoing,
+      {:ongoing, 5},
       name: :stats_manager
     )
   end
@@ -16,20 +16,30 @@ defmodule Zmobies.StatsManager do
     GenServer.cast(:stats_manager, {:async_read, requester, message})
   end
 
-  def handle_info(:check_status, _current_status) do
+  def handle_info(:check_status, {old_status, checks}) do
     new_status = World.status
 
-    if new_status == :ongoing || new_status == :empty do
-      send(self, :check_status)
-    else
-      GameManager.finish
+    new_checks = cond do
+      new_status == :ongoing || new_status == :empty ->
+        send(self, :check_status)
+        5
+      old_status == new_status && checks == 0 ->
+        GameManager.finish
+        0
+      old_status == new_status ->
+        send(self, :check_status)
+        checks - 1
+      true ->
+        send(self, :check_status)
+        # something is wrong. flush state and start over.
+        5
     end
 
-    {:noreply, new_status}
+    {:noreply, {new_status, new_checks}}
   end
 
-  def handle_cast({:async_read, requester, message}, status) do
+  def handle_cast({:async_read, requester, message}, state = {status, _}) do
     send(requester, {message, status})
-    {:noreply, status}
+    {:noreply, state}
   end
 end
