37c2d91b46df2d3a942bb2865d176f4248a9caed
Srinivas Rao <savinirs@gmail.com>
2016-12-28 23:03:18 -0800

    skeleton front-end

--- apps/web_client/web/static/js/app.js
+++ apps/web_client/web/static/js/app.js
@@ -12,10 +12,101 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function randomInt() {
+  return Math.ceil(Math.random() * 100)
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getRandomType() {
+  return Math.random() > 0.5 ? "zombie" : "human"
+}
+
+class Container extends React.Component {
+
+  constructor() {
+    super();
+    const beings = Array.apply(null, Array(600)).map(function(_, index) {
+      return {x: randomInt(), y: randomInt(), uuid: index, type: getRandomType()}
+    })
+    this.state = {beings: beings};
+  }
+
+  componentDidMount() {
+    setInterval(function() {
+      this.setState(
+        {
+          beings: this.state.beings.map(function(being) {
+            return Object.assign(being, {x: randomInt(), y: randomInt()})
+          })
+        }
+      )
+    }.bind(this), 3000)
+  }
+
+  renderChildren() {
+    return this.state.beings.map(function(being) {
+      return React.createElement("div",
+        {
+          key: being.uuid,
+          className: "being " + being.type,
+          style:
+            {
+              top: (being.y * 8) + "px",
+              left: (being.x * 8) + "px",
+              backgroundColor: being.color,
+            }
+        }
+      )
+    })
+  }
+
+  render() {
+    return (
+      React.createElement('div', {className: "container"},
+        React.createElement(FlipMove, {duration: 500},
+          this.renderChildren()
+        )
+      )
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Container, {}, []),
+  document.querySelector('anchor')
+);
