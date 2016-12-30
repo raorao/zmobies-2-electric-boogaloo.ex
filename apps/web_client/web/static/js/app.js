68960ac137e94901d8104741a2d4d8984ef8da84
Srinivas Rao <savinirs@gmail.com>
2016-12-30 10:44:47 -0800

    tuning

--- apps/web_client/web/static/js/app.js
+++ apps/web_client/web/static/js/app.js
@@ -27,7 +27,7 @@ channel.on("update", function(payload) {
   bus.push(payload)
 })
 
-let stream = bus.delay(1000).bufferingThrottle(100)
+let stream = bus.delay(2000).bufferingThrottle(110)
 
 class Container extends React.Component {
 
