8c61944c75ef4ee09f4346c2e9a043190d6d2814
Srinivas Rao <savinirs@gmail.com>
2017-01-02 11:29:19 -0800

    REVERT ME testing for websockets

--- apps/web_client/web/static/js/app.js
+++ apps/web_client/web/static/js/app.js
@@ -27,7 +27,7 @@ channel.on("update", function(payload) {
   bus.push(payload)
 })
 
-let stream = bus.delay(2000).bufferingThrottle(110)
+let stream = bus.delay(200).bufferingThrottle(20)
 
 class Container extends React.Component {
 
