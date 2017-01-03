4e808f7aed1b8127031b30373a798cc878611413
Srinivas Rao <savinirs@gmail.com>
2017-01-02 22:09:40 -0800

    re-disable the longform transport

--- apps/web_client/web/channels/user_socket.ex
+++ apps/web_client/web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule WebClient.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
-  transport :longpoll, Phoenix.Transports.LongPoll
+  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
